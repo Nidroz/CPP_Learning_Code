5a9fe73cb49c88d145575bd4a05daae570982ccb
Nidroz <na.sidhoum@gmail.com>
2024-03-17 00:29:09 +0100

    Chap 4 - Part 1 & Part 2 Done

--- chap-04/2-farm/FarmHouse.cpp
+++ chap-04/2-farm/FarmHouse.cpp
@@ -3,12 +3,17 @@
 #include "Chicken.h"
 #include "Cow.h"
 #include "Dog.h"
+#include <vector>
+#include "Opera.hpp"
 
 void sing_a_lot(const Animal& animal, unsigned int times)
 {
-    while (times-- > 0)
-    {
-        animal.sing();
+    if (times > 0) {
+        while (--times > 0)
+        {
+            animal.sing(' ');
+        }
+        animal.sing('\n');
     }
 }
 
@@ -24,6 +29,19 @@ int main()
     sing_a_lot(cow, 3u);
     sing_a_lot(dog, 1u);
     sing_a_lot(chicken, 2u);
+    
+    std::vector<Animal*> animals;
+    animals.push_back(&dog);
+    animals.push_back(&cat);
 
+    std::cout << "\nOne by one !" << std::endl;
+    for (const auto& animal : animals) {
+        animal->sing(' ');
+    }
+    std::cout << std::endl;
+
+    Opera opera;
+    std::cout << "\nOPERA !" << std::endl;
+    opera.sing();
     return 0;
 }
