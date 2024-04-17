f669cc289e2f1d62775f6ff56714081094ab7b21
Nidroz <na.sidhoum@gmail.com>
2024-04-17 17:14:08 +0200

    Chap5 - Part6/Tuples

--- chap-05/5-tuples.cpp
+++ chap-05/5-tuples.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <string>
+#include <tuple>
 
-bool parse_params(int argc, const char* const* argv, int& numerator, int& denominator)
+std::tuple<bool, int, int> parse_params(int argc, const char* const* argv)
 {
+    const std::tuple<bool, int, int> error {false, 0, 0};
     if (argc != 3)
     {
         std::cerr << "Program expects 2 parameters!" << std::endl;
-        return false;
+        return error;
     }
 
     std::string num_str = argv[1];
     std::string den_str = argv[2];
+
+    int numerator = 0;
+    int denominator = 0;
 
     try
     {
@@ -20,38 +25,36 @@ bool parse_params(int argc, const char* const* argv, int& numerator, int& denomi
     catch (const std::exception&)
     {
         std::cerr << "Program expects 2 integer parameters!" << std::endl;
-        return false;
+        return error;
     }
 
     if (denominator == 0)
     {
         std::cerr << "Denominator cannot be null!" << std::endl;
-        return false;
+        return error;
     }
 
-    return true;
+    return std::tuple { true, numerator, denominator };
 }
 
-int divide(int numerator, int denominator, int& reminder)
+std::tuple<int, int> divide(int numerator, int denominator)
 {
-    reminder = numerator % denominator;
-    return numerator / denominator;
+    return std::tuple { numerator / denominator, numerator % denominator};
 }
 
 int main(int argc, char** argv)
 {
-    int numerator   = 0;
-    int denominator = 0;
-
-    if (!parse_params(argc, argv, numerator, denominator))
+    const auto parse_res = parse_params(argc, argv);
+    if (!std::get<0>(parse_res))
     {
         return 1;
     }
 
-    int       reminder = 0;
-    const int quotient = divide(numerator, denominator, reminder);
+    const auto numerator = std::get<1>(parse_res);
+    const auto denominator = std::get<2>(parse_res);
+    const auto div_res = divide(numerator, denominator);
 
-    std::cout << numerator << " = " << denominator << " * " << quotient << " + " << reminder << std::endl;
+    std::cout << numerator << " = " << denominator << " * " << std::get<0>(div_res) << " + " << std::get<1>(div_res) << std::endl;
 
     return 0;
 }
