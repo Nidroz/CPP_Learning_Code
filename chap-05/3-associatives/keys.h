43bff382ef511673660b39324ed4d14012779d71
Nidroz <na.sidhoum@gmail.com>
2024-04-17 05:03:17 +0200

    Chap5 - Part3/Associatives

--- chap-05/3-associatives/keys.h
+++ chap-05/3-associatives/keys.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <functional>
 #include <string>
 
 class ComparableDog
@@ -7,8 +8,17 @@ class ComparableDog
 public:
     ComparableDog(const std::string& name, const std::string& species)
         : _name { name }
-        , _species { species }
-    {}
+        , _species { species } {}
+
+    bool operator<(const ComparableDog& other) const {
+        if (_name < other._name) {
+            return true;
+        } else if (_name > other._name) {
+            return false;
+        } else {
+            return _species < other._species;
+        }
+    }
 
 private:
     std::string _name;
@@ -20,10 +30,28 @@ class HashableDog
 public:
     HashableDog(const std::string& name, const std::string& species)
         : _name { name }
-        , _species { species }
-    {}
+        , _species { species } {}
+
+    size_t getHash() const {
+        std::hash<std::string> hash_fcn;
+        return hash_fcn(_name) ^ hash_fcn(_species);
+    }
+
+    bool operator==(const HashableDog& other) const {
+        return _name == other._name && _species == other._species;
+    }
 
 private:
     std::string _name;
     std::string _species;
 };
+
+namespace std {
+
+template <>
+struct hash<HashableDog>
+{
+    size_t operator()(const HashableDog& dog) const { return dog.getHash(); }
+};
+
+} // namespace std
--- chap-05/3-associatives/main.cpp
+++ chap-05/3-associatives/main.cpp
@@ -1,4 +1,58 @@
-int main()
-{
+#include "keys.h"
+
+#include <iostream>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+
+
+using Owner = std::pair<std::string, std::string>;
+
+int main() {
+    /*
+    std::set<ComparableDog> dogs;
+    ComparableDog           medor { "medor", "labrador" };
+    dogs.emplace(medor);
+
+    ComparableDog gus { "gus", "bordercollie" };
+    dogs.emplace(gus);
+    std::cout << dogs.size() << std::endl;
+
+    ComparableDog medor_clone = medor;
+    dogs.emplace(medor_clone);
+
+    std::cout << dogs.size() << std::endl;
+    */
+
+    /*
+    std::unordered_set<HashableDog> dogs;
+    
+    HashableDog medor { "medor", "labrador" };
+    dogs.emplace(medor);
+    std::cout << dogs.size() << std::endl;
+
+    HashableDog medorClone = medor;
+    dogs.emplace(medorClone);
+    std::cout << dogs.size() << std::endl;
+    */
+
+    // remplacer ComparableDog par HashableDog, si on veut utiliser une unordored_map plutôt qu'une map
+    std::map<ComparableDog, Owner> ownersByDog;
+    ComparableDog medor { "medor", "labrador" };
+    ComparableDog gus { "gus", "bordercollie" };
+    ComparableDog zim { "zim", "poodle" };
+    ComparableDog flippy { "flippy", "spaniel" };
+
+    ownersByDog.insert(std::make_pair(medor, Owner {"Tonton", "Adolf"}));
+    ownersByDog.insert_or_assign(gus, Owner {"Celine", "Noel"});
+    ownersByDog.emplace(zim, Owner {"3ami", "Staline"});
+    ownersByDog.try_emplace(flippy, "Sadam", "Hussein");
+
+    ownersByDog.emplace(gus, Owner { "Claire", "David" });
+    // -> gus is still owned by Marc (emplace does not modify values with existing keys, neither does insert or try_emplace)
+
+    ownersByDog.insert_or_assign(gus, Owner { "Vincent", "Nozick" });
+    // -> gus is now owned by Vincent (insert_or_assign reassign values with existing keys)
     return 0;
 }
